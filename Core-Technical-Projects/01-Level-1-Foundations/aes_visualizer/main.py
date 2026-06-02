371950bb7afe4c0f6461b9f49e066d5277160cb5
Bibhab Saha <bijanvkspv@gmail.com>
2026-06-02 18:54:11 +0530

    Implement basic encryption and decryption in main.py

--- Core-Technical-Projects/01-Level-1-Foundations/aes_visualizer/main.py
+++ Core-Technical-Projects/01-Level-1-Foundations/aes_visualizer/main.py
@@ -1,1 +1,25 @@
+#encrypt
+word = "SECRET"
+import random
+xor = random.randint(0,100)
+#print(xor)
+a = []
+for i in word:
+  a.append(str(ord(i) ^ xor).zfill(3))
+encrypt = "".join(str(i) for i in a)+str(bin(xor)[2:].zfill(10))
+print(f"The encrypted message is: {encrypt}")
+
+#decrypt
+rox = int(encrypt[-10:],2)
+#print(rox)
+l=[]
+for x in range(0,len(encrypt)-10,3):
+  var = int(encrypt[x:x+3])^rox
+  l.append(chr(var))
+  var = "".join(str(x) for x in l)
+print(f"{encrypt} is decoded as {var}")
+
+
+
+
 
