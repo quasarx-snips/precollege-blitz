7b8c5558e4c87ae8a6f2b4f63d4ea8a3e366d3a8
Bibhab Saha <bijanvkspv@gmail.com>
2026-06-02 18:02:14 +0530

    Implement basic linear regression training loop

--- Core-Technical-Projects/01-Level-1-Foundations/tiny_ml_math/main.py
+++ Core-Technical-Projects/01-Level-1-Foundations/tiny_ml_math/main.py
@@ -1,1 +1,15 @@
 
+data_set = [[1.0, 3.2], [2.0, 5.3], [3.0, 7.1]]
+multiplier = 0
+step_speed = 0.01
+bias = 0
+for i in range(10000):
+  for x,y in data_set:
+    prediction = (x*multiplier)+bias
+    error = y-prediction
+    multiplier += step_speed*error*x
+    bias += step_speed*error
+
+
+
+print(multiplier,bias)
