645344f57dd21c9fa6f6bc02ddf43adb6434d7f3
Bibhab Saha <bijanvkspv@gmail.com>
2026-06-03 22:13:16 +0530

    Update main.py

--- Core-Technical-Projects/01-Level-1-Foundations/aes_visualizer/main.py
+++ Core-Technical-Projects/01-Level-1-Foundations/aes_visualizer/main.py
@@ -1,25 +1,14 @@
-#encrypt
-word = "SECRET"
-import random
-xor = random.randint(0,100)
-#print(xor)
-a = []
-for i in word:
-  a.append(str(ord(i) ^ xor).zfill(3))
-encrypt = "".join(str(i) for i in a)+str(bin(xor)[2:].zfill(10))
-print(f"The encrypted message is: {encrypt}")
+# main.py
+from block_cipher import encrypt_message, decrypt_message
 
-#decrypt
-rox = int(encrypt[-10:],2)
-#print(rox)
-l=[]
-for x in range(0,len(encrypt)-10,3):
-  var = int(encrypt[x:x+3])^rox
-  l.append(chr(var))
-  var = "".join(str(x) for x in l)
-print(f"{encrypt} is decoded as {var}")
+def main():
+    word = input("Enter secret message: ")
+    encrypted = encrypt_message(word)
+    print(f"Encrypted: {encrypted}")
+    print(f"Decrypted: {decrypt_message(encrypted)}")
+
+if __name__ == "__main__":
+    main()
 
 
-
-
-
+#DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
