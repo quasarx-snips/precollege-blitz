c89d44c42e60cf732f1255b75fadfe272bbed9f1
Bibhab Saha <bijanvkspv@gmail.com>
2026-06-15 15:40:34 +0000

    git commit practise

---
+++ Core-Technical-Projects/02-Level-2-Simulations/cpu_emulator/git.bash
@@ -0,0 +1,8 @@
+# 1. Stage the files you changed
+git add Core-Technical-Projects/02-Level-2-Simulations/cpu_emulator/
+
+# 2. Save the changes locally with a message
+git commit -m "Update cpu emulator files"
+
+# 3. Force push to overwrite the remote branch
+git push origin main --force
